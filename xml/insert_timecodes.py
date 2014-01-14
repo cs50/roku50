fc0e27a3801dc7dc0167d04fa6b84c356f675fad
Colton <coltonoscopy@gmail.com>
2014-01-14 03:22:38 -0500

    made tweaks to xml script

--- xml/insert_timecodes.py
+++ xml/insert_timecodes.py
@@ -2,20 +2,22 @@ import xml.etree.ElementTree as ET
 import os
 
 dirs = ['lectures', 'shorts', 'psets', 'walkthroughs', 'seminars', 'quizzes', 'sections']
-years = ['2012']
+years = [2012]
 INDEX = 'index.xml'
+FALL_DIR = 'fall'
 
 # for each year in the years list, go through all category dirs and insert subtitle URLs
 for year in years:
 	for _dir in dirs:
 		# 2012 did not have a walkthroughs directory
-		if year == '2012' and _dir == 'walkthroughs':
+		if year == 2012 and _dir == 'walkthroughs':
 			continue
 
-		tree = ET.parse(os.path.join(year, 'fall', _dir, INDEX))
-		root = tree.getroot()
+		# grab the XML tree by opening a year/fall/dir/index.xml file
+		tree = ET.parse(os.path.join(year, FALL_DIR, _dir, INDEX))
 
-		feed = root
+		# grab root but also create a var called feed for clarity
+		feed = root = tree.getroot()
 
 		# for each item in the feed, if it is an item
 		for item in feed:
@@ -26,11 +28,12 @@ for year in years:
 			video_path = os.path.dirname(item[1].text)
 
 			# grab video name to append to dirs down below for srt path
+			# ex: lectures/0/week0w-720p.mp4 will turn into week0w
 			video_name = os.path.basename(item[1].text).split('-')[0]
 
-			subtitle_tag = ET.Element('subtitleURL')
+			subtitle_tag = ET.Element('subtitleUrl')
 
-			if int(year) <= 2012:
+			if year <= 2012:
 				english_dir = 'eng'
 			else:
 				english_dir = 'en'
